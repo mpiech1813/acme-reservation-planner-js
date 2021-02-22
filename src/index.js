156b0cedd8014814dbf763d0d4512f3f0c323588
Maciej Pie <maciejpiech18@gmail.com>
2021-02-22 14:43:35 -0600

    finished the skeleton

--- src/index.js
+++ src/index.js
@@ -14,11 +14,7 @@ const renderUsers = async () => {
             ${users
               .map(
                 (user) => `
-            <li> 
-                <a href="#${user.id}">
-                    ${user.name}
-                </a>
-            </li>
+            <li><a href="#${user.id}">${user.name}</a></li>
             `
               )
               .join('')}
@@ -37,9 +33,7 @@ const renderRestaurants = async () => {
             ${restaurants
               .map(
                 (restaurant) => `
-            <li> 
-                    ${restaurant.name}
-            </li>
+            <li>${restaurant.name}</li>
             `
               )
               .join('')}
@@ -50,15 +44,13 @@ const renderRestaurants = async () => {
   }
 };
 
-const renderReservations = async (reservations) => {
+const renderReservations = (reservations) => {
   try {
     const html = `
         ${reservations
           .map(
             (reservation) => `
-        <li> 
-                ${reservation.restaurant.name} @ ${reservation.createdAt}
-        </li>
+        <li>${reservation.restaurant.name} @ ${reservation.createdAt}</li><button data-id='${reservation.id}'>X</button>
         `
           )
           .join('')}
@@ -69,12 +61,19 @@ const renderReservations = async (reservations) => {
   }
 };
 
+async function getAllReservations(userId) {
+  const URL = `/api/users/${userId}/reservations`;
+  const reservations = (await axios.get(URL)).data;
+  return reservations;
+}
+
 window.addEventListener('hashchange', async () => {
   try {
     const userId = window.location.hash.slice(1);
-    const URL = `/api/users/${userId}/reservations`;
-    const reservations = (await axios(URL)).data;
-    renderReservations(reservations);
+
+    // const URL = `/api/users/${userId}/reservations`;
+    // const reservations = (await axios.get(URL)).data;
+    renderReservations(await getAllReservations(userId));
   } catch (err) {
     console.log(err);
   }
@@ -84,15 +83,43 @@ restaurantList.addEventListener('click', async (ev) => {
   const target = ev.target;
   const userId = window.location.hash.slice(1);
   let restaurantObj;
-
+  // console.log(userId);
   if (target.tagName === 'LI') {
     const restaurants = (await axios.get('/api/restaurants')).data;
-    restaurants.forEach((elem) => {
-      if (elem[name] === target.innerHTML) {
-        restaurantObj = elem;
+    restaurantObj = restaurants.find(function (elem) {
+      if (elem.name === target.innerHTML) {
+        return elem;
       }
     });
-    console.log(restaurantObj);
+    const _reservation = {
+      restaurantId: restaurantObj.id,
+    };
+
+    const response = (
+      await axios.post(`/api/users/${userId}/reservations`, _reservation)
+    ).data;
+    const reservationsArr = await getAllReservations(userId);
+    // console.log(typeof getAllReservations());
+    // console.log(`response: ${JSON.stringify(response)}`);
+    // console.log(`reservationsArr is ${JSON.stringify(reservationsArr)} `);
+    renderReservations(reservationsArr);
+  }
+});
+
+reservationList.addEventListener('click', async (ev) => {
+  const target = ev.target;
+  const userId = window.location.hash.slice(1);
+  if (target.tagName === 'BUTTON') {
+    const reservationId = target.getAttribute('data-id');
+    // console.log(reservationId);
+    const response = await axios.delete(`/api/reservations/${reservationId}`);
+
+    //get a lsit of all of the reservatiosn
+    // find the correct one
+    // pull out the id number
+    //send it to app.delete
+    const reservationsArr = await getAllReservations(userId);
+    renderReservations(reservationsArr);
   }
 });
 
